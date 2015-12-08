acaccf0be8a354728eb4e85ebc0c13a6d3b617d4
nuka <nuka@ebi.ac.uk>
2015-12-08 16:24:26 +0000

    add a username to the panther temp dir

--- core/management/src/main/java/uk/ac/ebi/interpro/scan/management/model/implementations/panther/PantherBinaryStep.java
+++ core/management/src/main/java/uk/ac/ebi/interpro/scan/management/model/implementations/panther/PantherBinaryStep.java
@@ -121,9 +121,12 @@ public final class PantherBinaryStep extends RunBinaryStep {
         String absolutePantherTempDirPath = this.getPerlScriptTempDir();
         LOGGER.debug("pantherTempDir - before update: " + absolutePantherTempDirPath);
         LOGGER.debug("temporaryFileDirectory: " + temporaryFileDirectory);
+        String username = System.getProperty("user.name").toLowerCase();
         if (! this.getPerlScriptTempDir().trim().isEmpty()) {
             if (new File(this.getPerlScriptTempDir()).isAbsolute()) {
                 absolutePantherTempDirPath = this.getPerlScriptTempDir()
+                        + File.separator
+                        + username
                         + File.separator
                         + PANTHER_TEMP_DIR_SUFFIX;
             }else{
@@ -131,6 +134,8 @@ public final class PantherBinaryStep extends RunBinaryStep {
                 absolutePantherTempDirPath = this.getUserDir()
                         + File.separator
                         + this.getPerlScriptTempDir()
+                        + File.separator
+                        + username
                         + File.separator
                         + PANTHER_TEMP_DIR_SUFFIX;
             }
