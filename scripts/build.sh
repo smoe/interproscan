908b58ac9bdb5f2b1dbbd9da9e1b133f9a62a054
Steffen Möller <moeller@debian.org>
2020-05-06 23:33:11 +0200

    Debian+Ubuntu offer ncoils package - hardening

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,7 @@
+#!/bin/sh
+
+set -e
+
 cd core 
 mvn -q clean install  
 cd jms-implementation 
@@ -5,18 +9,18 @@ mvn -q clean package
 cd target/interproscan-5-dist 
 cd $TRAVIS_BUILD_DIR/core/jms-implementation/target/interproscan-5-dist
 df -h  
-cd src/coils/ncoils/2.2.1
-make
-cd ../../../../
-cp src/coils/ncoils/2.2.1/ncoils bin/ncoils/2.2.1/
 wget ftp://ftp.ebi.ac.uk/pub/databases/interpro/iprscan/5/bin/centos7/rpsbproc.zip
 unzip rpsbproc.zip 
 cp rpsbproc bin/blast/ncbi-blast-2.9.0+/rpsbproc
 chmod +x bin/blast/ncbi-blast-2.9.0+/rpsbproc
 if which apt >/dev/null; then
   sudo apt install libdw1 libdw-dev
-fi
-if which yum >/dev/null; then
+  sudo apt install ncoils
+elif which yum >/dev/null; then
+  cd src/coils/ncoils/2.2.1
+  make
+  cd ../../../../
+  cp src/coils/ncoils/2.2.1/ncoils bin/ncoils/2.2.1/
   yum install libdw1 libdw-dev
 fi
 ./interproscan.sh -i test_proteins.fasta -f tsv -dp -appl sfld, hamap,prints,smart,pfam,pirsf,tigrfam,prositeprofiles,prositepatterns,gene3d,superfamily
